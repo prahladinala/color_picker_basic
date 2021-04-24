3fed3512691b496c05a49d45fcc1cf5705802636
Prahlad Inala <prahladinala@users.noreply.github.com>
2021-04-24 21:13:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+// Red Color
+function red1(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red2(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red3(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red4(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red5(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red6(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red7(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red8(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red9(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red10(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red11(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red12(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red13(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red14(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+function red15(){
+    const textarea = document.createElement('textarea');
+    textarea.value = "#E44236";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+
+
